bff46e85e4663287b36b2ff5ba5ee3006c4bf7a9
SeitaHigashi <higashi110902@gmail.com>
2018-05-27 19:56:07 +0900

    Elixir/Phoenix 初級② complete

--- projects/nano_planner/web/views/plan_items_view.ex
+++ projects/nano_planner/web/views/plan_items_view.ex
@@ -8,21 +8,33 @@ defmodule NanoPlanner.PlanItemsView do
 
   defp format_starts_at(item) do
     time_zone = Application.get_env(:nano_planner, :default_time_zone)
+    w = format_wday(item.starts_at)
     if item.starts_at.year == Timex.now(time_zone).year do
-      Strftime.format! item.starts_at, "%-m 月 %-d 日 %H:%M"
+      Strftime.format! item.starts_at, "%-m 月 %-d 日 (#{w}) %H:%M"
     else
-      Strftime.format! item.starts_at, "%Y 年 %-m 月 %-d 日 %H:%M"
+      Strftime.format! item.starts_at, "%Y 年 %-m 月 %-d 日 (#{w}) %H:%M"
     end
   end
 
   defp format_ends_at(item) do
+    w = format_wday(item.ends_at)
     cond do
       Timex.to_date(item.ends_at) == Timex.to_date(item.starts_at) ->
         Strftime.format! item.ends_at, "%H:%M"
-        item.ends_at.year == item.starts_at.year ->
-          Strftime.format! item.ends_at, "%-m 月 %-d 日 %H:%M"
-          true ->
-            Strftime.format! item.ends_at, "%Y 年 %-m 月 %-d 日 %H:%M"
-      end
+      item.ends_at.year == item.starts_at.year ->
+        Strftime.format! item.ends_at, "%-m 月 %-d 日 (#{w}) %H:%M"
+      true ->
+        Strftime.format! item.ends_at, "%Y 年 %-m 月 %-d 日 (#{w}) %H:%M"
     end
+  end
+
+  def format_datetime(datetime) do
+    w = format_wday(datetime)
+    Strftime.format! datetime, "%Y 年 %-m 月 %-d 日 (#{w}) %H:%M"
+  end
+
+  defp format_wday(datetime) do
+    Enum.at ~w( 日 月 火 水 木 金 土 ),
+    Timex.days_to_beginning_of_week(datetime, :sun)
+  end
 end
